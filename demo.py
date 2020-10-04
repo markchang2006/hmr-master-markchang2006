ad0c004a3bc60c2428b93b11ae8da38958933074
markchang2006 <markchang2006@users.noreply.github.com>
2020-10-04 22:06:25 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -26,8 +26,8 @@ import numpy as np
 import skimage.io as io
 import tensorflow as tf
 
-    print (src.util)
-    print (render)
+print (src.util)
+print (render)
 
 from src.util import renderer as vis_util
 from src.util import image as img_util
