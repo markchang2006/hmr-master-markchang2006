650ad03ee1a8e472dfcf45c61350af33bfe2968b
markchang2006 <markchang2006@users.noreply.github.com>
2020-10-05 00:33:36 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -20,8 +20,8 @@ import numpy as np
 
 #curr_path = osp.dirname(osp.abspath(__file__))
 #model_dir = osp.join(curr_path,'models')
-curr_path = ('/content/keras_Realtime_Multi-Person_Pose_Estimation/hmr-master-markchang2006/src')
-model_dir = ('/content/keras_Realtime_Multi-Person_Pose_Estimation/hmr-master-markchang2006/src/models')
+curr_path = ('/content/keras_Realtime_Multi-Person_Pose_Estimation/hmr_master_markchang2006/src')
+model_dir = ('/content/keras_Realtime_Multi-Person_Pose_Estimation/hmr_master_markchang2006/src/models')
 if not osp.exists(model_dir):
     print('Fix path to models/')
     print(curr_path,model_dir)
@@ -63,7 +63,7 @@ flags.DEFINE_string(
 # Training settings:
 # TODO! If you want to train, change this to your 'tf_datasets' or specify it with the flag.
 #DATA_DIR = '/scratch1/projects/tf_datasets/'
-DATA_DIR = '/hmr-master-markchang2006/src/tf_smpl/'
+DATA_DIR = '/hmr_master_markchang2006/src/tf_smpl/'
 
 flags.DEFINE_string('data_dir', DATA_DIR, 'Where to save training models')
 flags.DEFINE_string('log_dir', 'logs', 'Where to save training models')
