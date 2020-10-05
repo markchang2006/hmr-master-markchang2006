12967983a4cb34548767aff9d02fcb9418c54b36
markchang2006 <markchang2006@users.noreply.github.com>
2020-10-05 23:18:57 +0800

    Update renderer.py

--- src/util/renderer.py
+++ src/util/renderer.py
@@ -13,11 +13,9 @@ import sys
 #sys.path.extend(['/content/models/research/', '/content/models/research/slim/'])
 sys.path.insert(1, '../polmorenoc/opendr/opendr/')
 #sys.path.append('/polmorenoc/opendr/opendr')
-print (sys.path)
-
 
 #from camera.opendr import ProjectPoints
-import ProjectPoints
+import ProjectPoints.camera
 #from camera import ProjectPoints
 from renderer import ColoredRenderer
 from lighting import LambertianPointLight
