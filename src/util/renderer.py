f40143ab9c4bf455df7dad8ffce079f0f136782b
markchang2006 <markchang2006@users.noreply.github.com>
2020-10-06 02:02:56 +0800

    Update renderer.py

--- src/util/renderer.py
+++ src/util/renderer.py
@@ -14,6 +14,7 @@ import cv2
 #sys.path.insert(1, '../polmorenoc/opendr/opendr/')
 #sys.path.append('/content/polmorenoc/opendr/opendr')
 
+pip install pip
 pip install import_from_github_com
 from github_com.polmorenoc.opendr.opendr.camera import ProjectPoints
 
