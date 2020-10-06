aee78bda74a9b8920ac1fed3c3da17f76fd87f31
markchang2006 <markchang2006@users.noreply.github.com>
2020-10-07 00:12:00 +0800

    Update renderer.py

--- src/util/renderer.py
+++ src/util/renderer.py
@@ -13,7 +13,6 @@ import cv2
 #sys.path.extend(['/content/models/research/', '/content/models/research/slim/'])
 #sys.path.insert(1, '../polmorenoc/opendr/opendr/')
 #sys.path.append('/content/polmorenoc/opendr/opendr')
-python -m pip --version
 
 from github_com.polmorenoc.opendr.opendr.camera import ProjectPoints
 
